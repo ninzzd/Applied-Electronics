df6bad779b11a68f6dd47fa77d5ce971a25c6ee6
ninzzd <ninaad.desai1212@gmail.com>
2025-04-09 05:03:39 +0530

    Adding the Signals and Systems Practical Implementations Directory

---
+++ Signals-and-Systems/FFT.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cmath
+
+def fft(x:list):
+    n = np.log2(len(x))
+    if(n == 0):
+        return x
+    elif(np.floor(n) == n):
+        g = []
+        h = []
+        for i in range(0,int(len(x)/2)):
+            g.append(x[2*i])
+            h.append(x[2*i+1])
+        g_hat = fft(g)
+        h_hat = fft(h)
+        x_hat = []
+        for i in range(0,int(len(x)/2)):
+            x_hat.append(g_hat[i] + h_hat[i]*cmath.exp(-2*np.pi*1j/n))
+        for i in range(0,int(len(x)/2)):
+            x_hat.append(g_hat[i] - h_hat[i]*cmath.exp(-2*np.pi*1j/n))
+        return x_hat
+
+t = np.linspace(0,2*np.pi,int(np.exp2(10.0)))
+# x1 = []
+# for i in t:
+#     if(i>=-np.pi/2 and i <= np.pi/2):
+#         x1.append(1.0)
+#     else:
+#         x1.append(0.0)
+# x1 = np.array(x1)
+x1 = np.sin(t)
+x1_hat = np.array(fft(x1.tolist()))
+
+
+plt.plot(t,x1)
+plt.xlabel('Time(t)')
+plt.ylabel('x(t)')
+plt.show()
+
+plt.plot(t,x1_hat)
+plt.xlabel('Frequency(f)')
+plt.ylabel('X(f)')
+plt.show()
---
+++ Signals-and-Systems/fourier_series_approximation.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 1024
+
+def convolve(x,y):
+    g = []
+    for j in range(0,N):
+        sum = 0
+        for i in range(0,N):
+            # Limit of sum integral approximation to continuous-time convolution (circular)
+            sum+=x[i]*y[(j-i+int(N/2))%N]/N
+        g.append(sum)
+    return g
+
+def dirichletKernel(n:int,t):
+    d_n = np.zeros_like(t)
+    d_n[t != 0] = np.sin(t*(n+0.5))/(np.sin(t/2))
+    d_n[t == 0] = (2*n + 1)
+    return d_n
+
+def fejerKernel(n:int,t):
+    f_n = np.zeros_like(t)
+    for i in range(0,n):
+        f_n = f_n + dirichletKernel(i,t)
+    f_n = f_n/n
+    return f_n
+
+t = np.linspace(-np.pi,np.pi,N)
+x = np.sin(t)
+y = np.sign(t)
+
+# 1 - Square-wave
+# x1 = []
+# for i in t:
+#     if(i>=-np.pi/2 and i <= np.pi/2):
+#         x1.append(1.0)
+#     else:
+#         x1.append(0.0)
+
+# x1 = np.array(x1)
+
+
+# 2 - Saw-tooth
+x1 = []
+for i in t:
+    if(i>=-np.pi and i<0):
+        x1.append(1*(i+np.pi)/np.pi)
+    else:
+        x1.append(1*(i)/np.pi)
+x1 = np.array(x1)
+
+n = 100
+
+d_n = dirichletKernel(n,t)
+
+f_n = fejerKernel(n,t)
+    
+s_n_1 = convolve(x1,d_n)
+
+s_n_2 = convolve(x1,f_n)
+
+plt.plot(t,x1)
+plt.ylabel('x(t)')
+plt.xlabel('t')
+plt.xlim(right = +np.pi,left = -np.pi)
+plt.show()
+
+plt.plot(t,d_n)
+plt.ylabel('Dn(t)')
+plt.xlabel('t')
+plt.xlim(right = +np.pi,left = -np.pi)
+plt.show()
+
+plt.plot(t,f_n)
+plt.ylabel('Fn(t)')
+plt.xlabel('t')
+plt.xlim(right = +np.pi,left = -np.pi)
+plt.show()
+
+
+plt.plot(t,s_n_1)
+plt.ylabel('Sn(t) [Dirichlet]')
+plt.xlabel('t')
+plt.xlim(right = +np.pi,left = -np.pi)
+plt.show()
+
+plt.plot(t,s_n_2)
+plt.ylabel('Sn(t) [Fejer]')
+plt.xlabel('t')
+plt.xlim(right = +np.pi,left = -np.pi)
+plt.show()
